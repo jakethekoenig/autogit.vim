d4585378caa05ff84564df4dba3d69556affdb4c
MentatBot <160964065+MentatBot@users.noreply.github.com>
2025-02-14 16:40:41 +0000

    Fix precommit script to only check plugin files
    Modified precommit.sh to only check Vim files in the plugin directory, excluding documentation files which aren't meant to be sourced as Vimscript. This should prevent false positives from documentation syntax.
    
    Mentat precommits passed. Log: https://mentat.ai/log/6f929713-8287-44dc-8ee4-afc52a5134be

--- .mentat/precommit.sh
+++ .mentat/precommit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Since this is a Vim plugin, we'll just do basic file checks
-for file in $(git ls-files "*.vim"); do
+# Check only plugin files, not documentation
+for file in $(git ls-files "plugin/*.vim"); do
     if ! vim -esN -u NONE -U NONE -i NONE --cmd "try | source $file | catch | cquit | endtry | quit"; then
         echo "Vim syntax error in $file"
         exit 1
